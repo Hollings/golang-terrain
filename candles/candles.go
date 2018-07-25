945e431fc0555cfbf947790319d5293b7cb446cb
John Hollingsworth <hollingsfsdf@gmail.com>
2018-07-25 11:47:18 -0600

    Added basic flame, seeded RNG

--- candles/candles.go
+++ candles/candles.go
@@ -11,12 +11,15 @@ import (
     "github.com/skratchdot/open-golang/open"
     "github.com/ojrac/opensimplex-go"
     // "image/gif"
+    "time"
 
 )
 var (
 	colorGrey = color.RGBA{150,150,150,255}
     colorWhite = color.RGBA{255,255,255,255}
     colorBrown = color.RGBA{139,69,19,255}
+    colorYellow = color.RGBA{255,255,0,255}
+
     colorDarkGrey = color.RGBA{100,100,100,255}
     colorLightGrey = color.RGBA{200,200,200,255}
     colorDarkBrown = color.RGBA{100,60,10,255}
@@ -26,7 +29,7 @@ var (
 
 func main() {
 	fmt.Println("starting")
-    img := image.NewRGBA(image.Rect(0, 0, 1000, 1000))
+    img := image.NewRGBA(image.Rect(0, 0, 1000, 200))
 
     // Fill the canvas with black
     for i := 0; i < 1000; i++ {
@@ -35,14 +38,18 @@ func main() {
     	}
     }
 
-    for o := 50; o < 1000; o+=50 {
+    // This will be where the center point of the candles are
+    originHeight := 100
 
-	    baseHeight := rand.Intn(100)+10
-	   	candleWidth := rand.Intn(20)
-	    candleHeight := rand.Intn(100)
+    rand.Seed(time.Now().UTC().UnixNano())
+
+    for o := 50; o < 1000; o+=50 {
+	    baseHeight := rand.Intn(25)+10
+	   	candleWidth := rand.Intn(15)
+	    candleHeight := rand.Intn(50)
 	    // These are for later
-	    //wickHeight := rand.Intn(10)
-	    //flameHeight := rand.Intn(10)
+	    wickHeight := rand.Intn(10)
+	    flameAmount := rand.Intn(3)+1
 	    //candleBoundsX = candleWidth
 	    //candleBoundsY = candleHeight + baseHeight + wickHeight + flameHeight
 	    
@@ -54,7 +61,7 @@ func main() {
 
 	    		// Creates a smooth gradient from 100 - 150
 	    		// Im not sure if I like how non-pixel art this looks
-				img.Set(o+i+2,51-z,color.RGBA{uint8(150.0-50.0*(float32(i)/float32(candleWidth))),
+				img.Set(o+i+2,(originHeight+1)-z,color.RGBA{uint8(150.0-50.0*(float32(i)/float32(candleWidth))),
 											  uint8(150.0-50.0*(float32(i)/float32(candleWidth))),
 											  uint8(150.0-50.0*(float32(i)/float32(candleWidth))),255})    
 
@@ -63,9 +70,18 @@ func main() {
 
 	    // Create candle "holder"
 	    for i := candleWidth+4; i > 0; i-- {
-			img.Set(o+i,50,colorDarkBrown)    	
+			img.Set(o+i,originHeight,colorDarkBrown)    	
 	    }
 	   
+	   // Create the wick
+	    for i := 0; i < wickHeight; i++ {
+			img.Set(o+(candleWidth/2)+3,(originHeight+1)-(i+candleHeight),colorDarkGrey)    	
+	    }
+
+	    // Create Flame
+	    for i := 0; i < flameAmount; i++ {
+			img.Set(o+(candleWidth/2)+3,(originHeight+1)-(i+wickHeight+candleHeight),colorYellow)    	
+	    }
 
 	    // Randomly shape the candle holder thing
 
@@ -73,7 +89,7 @@ func main() {
 	    currentSides := [2]int{3+o,o+2+candleWidth}
 	    lastDirection := 0.0;
 	    lastColor := colorBrown;
-	  	simp := opensimplex.NewWithSeed(int64(o+1212));
+	  	simp := opensimplex.NewWithSeed(time.Now().UTC().UnixNano());
 
 	  	// Use the simplex noise to random walk the edges for the entire height of the base.
 	  	// I'm using 0.3 and -0.3 as the threshold for left and right. Might smooth it out more.
@@ -88,12 +104,12 @@ func main() {
 	    	}
 
 	    	// Set the outline
-    		img.Set(currentSides[0],51+i,colorBrown)    	
-    		img.Set(currentSides[1],51+i,colorDarkBrown)
+    		img.Set(currentSides[0],originHeight+1+i,colorBrown)    	
+    		img.Set(currentSides[1],originHeight+1+i,colorDarkBrown)
 
     		// Set the fill
     		for z := currentSides[0]+1; z < currentSides[1]; z++ {
-    			img.Set(z,51+i,lastColor)    	
+    			img.Set(z,originHeight+1+i,lastColor)    	
     		}
 	    	lastDirection = direction
 
